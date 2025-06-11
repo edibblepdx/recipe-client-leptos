5a784240fcf090c987481ef8822c82f6f85c7429
ethan dibble <edibble@pdx.edu>
2025-06-11 14:48:21 -0700

    recipe stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
+mod recipe;
+
+use crate::recipe::Recipe;
+
 use leptos::prelude::*;
 use leptos::{ev::SubmitEvent, html};
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (input, set_input) = signal("".to_string());
-
-    view! {
-        <FormSubmit
-            set_input=set_input
-            text="default"
-        />
-        <p>{input}</p>
-    }
+    view! { <FetchRecipe/> }
 }
 
 /// Form Submit that gives input to some outer scope
@@ -23,7 +19,7 @@ fn FormSubmit<'a>(
     set_input: WriteSignal<String>,
     /// default text
     #[prop(optional)]
-    text: &'a str,
+    placeholder: &'a str,
 ) -> impl IntoView {
     let input_element: NodeRef<html::Input> = NodeRef::new();
 
@@ -41,10 +37,109 @@ fn FormSubmit<'a>(
     view! {
         <form on:submit=on_submit>
             <input type="text"
-                placeholder=text
+                placeholder=placeholder
                 node_ref=input_element
             />
             <input type="submit" value="Submit"/>
         </form>
+    }
+}
+
+/// based on Leptos crate fetch example
+#[component]
+fn FetchRecipe() -> impl IntoView {
+    let (endpoint, set_endpoint) = signal::<String>("random".to_string());
+    let recipe = LocalResource::new(move || recipe::fetch(endpoint.get()));
+
+    let fallback = move |errors: ArcRwSignal<Errors>| {
+        let error_list = move || {
+            errors.with(|errors| {
+                errors
+                    .iter()
+                    .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+                    .collect::<Vec<_>>()
+            })
+        };
+
+        view! {
+            <div class="error">
+                <h2>"Error"</h2>
+                <ul>{error_list}</ul>
+            </div>
+        }
+    };
+
+    let (input, set_input) = signal("".to_string());
+
+    view! {
+        <div>
+            <Transition fallback=|| view! { <div>"Loading..."</div> }>
+                <ErrorBoundary fallback>
+                {move || Suspend::new(async move {
+                    recipe.map(|r| {
+                        match r {
+                            Ok(r) => view! { <CommonRecipe r=r/> }.into_any(),
+                            Err(_) => view! { <DefaultRecipe/> }.into_any(),
+                        }
+                    })
+                })}
+                </ErrorBoundary>
+            </Transition>
+        </div>
+        <div class="new-recipe">
+            <h4>"New Recipe"</h4>
+            <FormSubmit
+                set_input=set_input
+                placeholder="cuisine (optional)"
+            />
+        </div>
+    }
+}
+
+#[component]
+fn CommonRecipe<'a>(r: &'a Recipe) -> impl IntoView {
+    view! {
+        <div class="recipe">
+            <span class="name">{r.name.clone()}</span><br/>
+            <span class="field">"cuisine: "{r.cuisine.clone()}</span><br/>
+            <span class="field">"cook time: "{r.cooking_time_minutes}</span><br/>
+            <span class="field">"prep time: "{r.prep_time_minutes}</span><br/>
+            <span class="field">"servings: "{r.servings}</span><br/>
+            <span class="field">"calories: "{r.calories_per_serving}</span><br/>
+            <h4>"Ingredients"</h4>
+            <ul>
+                { r.ingredients.iter().map(|i| {
+                    view! { <li> { i.clone() } </li> }
+                }).collect::<Vec<_>>()}
+            </ul>
+            <h4>"Dietary Restrictions"</h4>
+            <ul>
+                { r.dietary_restrictions.iter().map(|i| {
+                    view! { <li> { i.clone() } </li> }
+                }).collect::<Vec<_>>()}
+            </ul>
+        </div>
+    }
+}
+
+#[component]
+fn DefaultRecipe() -> impl IntoView {
+    view! {
+        <div class="recipe">
+            <span class="name">"Not Found"</span><br/>
+            <span class="field">"cuisine: None"</span><br/>
+            <span class="field">"cook time: None"</span><br/>
+            <span class="field">"prep time: None"</span><br/>
+            <span class="field">"servings: None"</span><br/>
+            <span class="field">"calories: None"</span><br/>
+            <h4>"Ingredients"</h4>
+            <ul>
+                <li>"None"</li>
+            </ul>
+            <h4>"Dietary Restrictions"</h4>
+            <ul>
+                <li>"None"</li>
+            </ul>
+        </div>
     }
 }
---
+++ src/recipe.rs
@@ -0,0 +1,25 @@
+// Based on Leptos crate fetch example
+
+use leptos::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Recipe {
+    pub id: i64,
+    pub name: String,
+    pub cuisine: String,
+    pub ingredients: Vec<String>,
+    pub cooking_time_minutes: i64,
+    pub prep_time_minutes: i64,
+    pub servings: i64,
+    pub calories_per_serving: i64,
+    pub dietary_restrictions: Vec<String>,
+}
+
+pub async fn fetch(endpoint: String) -> Result<Recipe, Error> {
+    use reqwasm::http::Request;
+
+    let endpoint = format!("http://localhost:3000/api/vi/recipe/{}", endpoint);
+    let result = Request::get(&endpoint).send().await?.json().await?;
+    Ok(result)
+}
